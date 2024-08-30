ebdfff5f02c59ed2b2c07b07fdf8a095f02d62e5
jhondiaz18 <jhon_0621@hotmail.com>
2024-08-30 17:25:19 -0500

    Add project files.

---
+++ WebApi/Startup.cs
@@ -0,0 +1,41 @@
+﻿using BussinesLogic.Logic;
+using Core.Interfaces;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace WebApi;
+
+public class Startup
+{
+    public IConfiguration Configuration { get; set; }
+
+    public Startup(IConfiguration configuration)
+    {
+        Configuration = configuration;
+    }
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddTransient<IProductoRepository, ProductoRepository>();
+        services.AddControllers();
+    }
+
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        if (env.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+        }
+
+        app.UseRouting();
+        app.UseAuthorization();
+        app.UseAuthentication();
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapControllers();
+        });
+    }
+}
